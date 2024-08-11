a696343687330dcea89a3da92ee971cd499740df
Harsh Agarwal <harshag.code@gmail.com>
2024-08-12 01:09:16 +0530

    venv

--- MLOps/Packaging-ML-Model/packaging-ml-model/prediction_model/predict.py
+++ MLOps/Packaging-ML-Model/packaging-ml-model/prediction_model/predict.py
@@ -1,5 +1,16 @@
 import numpy as np
 import pandas as pd
+
+from pathlib import Path
+import os
+import sys
+# This makes sure our package is found
+PACKAGE_ROOT = Path(os.path.abspath(os.path.dirname(__file__))).parent
+sys.path.append(str(PACKAGE_ROOT))
+
+import warnings
+warnings.filterwarnings('ignore')
+
 from prediction_model.config import config
 from prediction_model.processing.data_handling import load_pipeline, load_dataset
 
@@ -18,6 +29,7 @@ def generate_predictions():
     test_data = load_dataset(config.TEST_FILE)
     preds = model_pipeline.predict(test_data[config.FEATURES])
     output = np.where(preds == 1, 'Y', 'N')
+    print(output)
     # result = {'Predictions': output}
     return output
 
