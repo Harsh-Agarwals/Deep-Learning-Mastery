aff268e2de054708045c2eb724b467376f220b88
Harsh Agarwal <harshag.code@gmail.com>
2024-08-22 23:47:04 +0530

    .

--- MLOps/Packaging-ML-Model/packaging-ml-model/loan_prediction_flask.py
+++ MLOps/Packaging-ML-Model/packaging-ml-model/loan_prediction_flask.py
@@ -1,25 +1,53 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
 from prediction_model.predict import predict_values
 
 app = Flask(__name__)
 
+def transform_to_integers(data):
+    columns_to_convert = ['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount']
+
+    for col in columns_to_convert:
+        try:
+            data[col] = int(data[col])
+        except ValueError:
+            print(f"Warning: Could not convert '{col}' to integer. Value: {data[col]}")
+    return data
+
+def transform_to_float(data):
+    columns_to_convert = ['Loan_Amount_Term', 'Credit_History']
+
+    for col in columns_to_convert:
+        try:
+            data[col] = float(data[col])
+        except ValueError:
+            print(f"Warning: Could not convert '{col}' to integer. Value: {data[col]}")
+    return data
+
 @app.route("/")
 def home():
-    return '<h3>{"Message": "Welcome to Loan Prediction app"}</h3><h4>To go to app: /loan-prediction</h4>'
+    return render_template("index.html")
 
-@app.route("/loan-prediction", methods=['GET'])
+@app.route("/loan-prediction", methods=['POST'])
 def home_page():
-    if request.method == 'GET':
-        if request.args.get("submit") == None:
-            return render_template("index.html")
-        elif request.args.get("submit") == "":
-            return "<h1>INVALID, please try again!</h1>"
-        else:
+    try:
+        if request.method == 'POST':
             data = {}
             for i in request.args:
                 data[i] = request.args.get(i)
-            df = pd.DataFrame([data])
-            print(df)
-            result = predict_values({data})
-            print(result)
+            del data['submit']
+            data = transform_to_integers(data)
+            data = transform_to_float(data)
+            df = pd.DataFrame([data]).iloc[0, :]
+            result = predict_values([df])
+            prediction = 'Eligible' if result['Predictions'][0] == 'Y' else 'Ineligible'
+            return jsonify(final=prediction)
+        else:
+            return jsonify(error="Invalid request method. Only POST is allowed."), 405
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return str(e)
+        
+if __name__ == "__main__":
+    app.run(debug=True)
