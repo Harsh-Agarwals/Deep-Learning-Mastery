4f7f8b867afede3984106f9cd831c986ebde9f4c
Harsh Agarwal <harshag.code@gmail.com>
2024-11-21 23:46:25 +0530

    'langserve'

--- LLM/Langchain/Projects/serve.py
+++ LLM/Langchain/Projects/serve.py
@@ -1,4 +1,5 @@
 import os
+import uvicorn
 from dotenv import load_dotenv
 
 from fastapi import FastAPI
@@ -12,6 +13,12 @@ from langchain_core.messages import HumanMessage, SystemMessage
 load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
+app = FastAPI(
+    title="Story Generator App",
+    version="1.0.0",
+    description="Provide a genre with a topic, and this will output a small story based on topic for this genre."
+)
+
 model = ChatGroq(
     model="gemma2-9b-it",
     temperature=0.3,
@@ -19,11 +26,27 @@ model = ChatGroq(
     api_key=GROQ_API_KEY
 )
 
+# prompt = ChatPromptTemplate.from_messages([
+#     SystemMessage(content="You are an AI system that makes {genre} story from the key provided by the user"),
+#     HumanMessage(content="{key}")
+# ])
+
 prompt = ChatPromptTemplate.from_messages([
-    SystemMessage(content="You are an AI system that makes {genre} story from the key provided by the user"),
-    HumanMessage(content="{key}")
+    ("system", "You are an AI system that makes {genre} story from the key provided by the user"),
+    ("user", "{key}")
 ])
 
 parser = StrOutputParser()
 
 chain = prompt | model | parser
+
+# FastAPI routes 
+add_routes(
+    app,
+    chain,
+    path="/story"
+)
+
+if __name__=="__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
